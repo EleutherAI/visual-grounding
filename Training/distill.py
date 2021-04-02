9637f414a40bcf759e4473e9b252b9cc891908b5
Leo Gao <leogao31@gmail.com>
2021-04-02 17:22:52 -0600

    Implement AR loss and fix a bunch of bugs

--- Training/distill.py
+++ Training/distill.py
@@ -3,6 +3,7 @@ from transformers import GPTNeoModel, GPTNeoForCausalLM,\
 from torch.utils.data import IterableDataset, DataLoader 
 from lm_dataformat import *
 import torch
+import torch.nn.functional as F
 from torch.nn.functional import normalize, cross_entropy
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -33,7 +34,7 @@ class DistillDataset(IterableDataset):
     def __init__(self,\
         tokenizer, clip_batch_size,
         clip_dataset_dir, pile_dataset_dir,
-        special_token = "<CLIP>", steps = 1e6):
+        special_token = "<|CLIP|>", steps = 1e6):
         self.clip_dataset_dir = clip_dataset_dir
         self.pile_dataset_dir = pile_dataset_dir
 
@@ -76,7 +77,7 @@ class DistillDataset(IterableDataset):
             for _ in range(self.clip_batch_size):
                 text, img_latent=next(self.clip_rdr)
                 #Append special token
-                text += "<CLIP>"
+                text += "<|CLIP|>"
                 txts.append(text)
                 img_latents.append([img_latent])
 
@@ -85,7 +86,7 @@ class DistillDataset(IterableDataset):
         #Get the index of the clip tokens.
         clip_idx = torch.sum(toks.attention_mask, dim=-1) - torch.tensor([1] * len(txts)).to(device)
         #Get latent vectors
-        latents = torch.stack([torch.tensor(x) for x in img_latents]).to(device)
+        latents = torch.cat([torch.tensor(x) for x in img_latents], dim=0).to(device)
 
         cc = self.cur_clip
         #Flip cur clip
@@ -113,6 +114,24 @@ def clip_loss(a, b, temp):
     
     return loss / 2.0
 
+def ar_loss(model, inp, attn_mask):
+    # inp :: [b, seq]
+    logprobs = F.log_softmax(model(inp, attention_mask=attn_mask, return_dict=True)['logits'], dim=-1)
+    # logprobs :: [b, seq, vocab]
+
+    pred = logprobs[:, :-1]
+    tgt = inp[:, 1:]
+
+    is_clip_or_padding_token = tgt >= 50257
+
+    logits = torch.gather(pred, 2, tgt.unsqueeze(-1)).squeeze(-1) # [batch, seq-1]
+
+    # remove loss of clip-token
+    logits *= 1 - is_clip_or_padding_token.to(torch.int)
+
+    return logits.sum()
+
+
 #Load dataset
 data = DistillDataset(tokenizer = tokenizer, clip_batch_size = 8,\
     clip_dataset_dir = "../clip_latents_100k.jsonl.zst",\
@@ -130,23 +149,30 @@ for batch, data_elem in enumerate(data):
     }
     #Used for CLIP. TODO: Fetch AR loss (Leo pls do this)
     out_embeds = model(**model_input, return_dict=True, output_hidden_states=True)['hidden_states']
+    
+    # debug shapes
+    #print([(k, v.shape if isinstance(v, torch.Tensor) else v) for k, v in data_elem.items()])
+
     #If we are currently using contrastive loss
     if data_elem['use_distill']:
         #out_embeds ~ (b x seq_len x hidden_size)
         idx = data_elem['clip_idx']
-        last_layer = out_embeds[0]
+        last_layer = out_embeds[-1] # -1 for last layer
+
         #Get predicted clip embedding. Grab from sequence_len dimension
         clip_embeds = torch.zeros((data.clip_batch_size, neo_hidden)).to(device)
         for i,j in enumerate(idx):
             clip_embeds[i] = last_layer[i][j]
         #Project to the correct size
         clip_embeds = projection(clip_embeds)
-        #Compute loss
+        #Compute contrastive loss
         loss = clip_loss(clip_embeds,  data_elem['latent_vecs'], temp_tensor)
-        #loss += ar_loss_with_clip_masked #Leo pls
-    else:
-        pass
-        #Do AR loss only
+
+    #compute AR loss
+    n_text_toks = data_elem['clip_idx'].sum()
+    loss += ar_loss(model, data_elem['input_ids'], data_elem['attention_mask']) / n_text_toks
+
+    print(float(loss))
     loss.backward()
     if (batch+1)%grad_accum==0:
         opt.step()
