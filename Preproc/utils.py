25006619d92daaa1ea3861dc4590c950096a4217
Leo Gao <54557097+leogao2@users.noreply.github.com>
2021-04-21 20:39:25 -0600

    Add User-Agent

--- Preproc/utils.py
+++ Preproc/utils.py
@@ -48,7 +48,10 @@ def curl(x):
     cooldown = 1
     for i in range(10):
         try:
-            response = urllib.request.urlopen(x, timeout=30)
+            response = urllib.request.urlopen(x, timeout=30, 
+                            headers={
+                                'User-Agent': 'WebImageText-collector/0.0 (https://github.com/EleutherAI/visual-grounding/; contact@eleuther.ai) pyfra/0.0'
+                            })
             data = response.read()
         except urllib.error.URLError as e:
             if e.code == 429:
